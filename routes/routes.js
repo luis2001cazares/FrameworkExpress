9df5ba4b80b1ac88f492dffc2caa019547893057
Luis David Cazares Morales <luisdavidcazares@gmail.com>
2023-05-09 08:35:19 -0600

    cambios mínimos en las rutas de users

--- routes/routes.js
+++ routes/routes.js
@@ -8,44 +8,63 @@ const router = app => {
             message: 'Bienvenido a Node.js Express REST API!'
         });
     });
-    //mostrar todos los usuarios
-    app.get('/users', (request, response) => {
-        pool.query('SELECT * FROM users', (error, result) => {
-            if (error) throw error;
-            response.send(result);
-        });
-    });
-    //mostrar un solo usuario por ID
-    app.get('/users/:id', (request, response) => {
-        pool.query('SELECT * FROM users WHERE id = ?', (error, result) => {
-            if (error) throw error;
-            response.send(result);
-        });
-    });
+     //Mostrar todos los usuarios
+  app.get("/users", (request, response) => {
+    pool.query("SELECT * FROM users", (error, result) => {
+      if (error) throw error;
 
-    //agregar un nuevo usuario
-    app.post('/users', (request, response) => {
-        pool.query('INSERT INTO users SET ?', request.body, (error, result) => {
-            if (error) throw error;
-            response.status(201).send('User added with ID: ${result.insertId}');
-        });
+      response.send(result);
     });
-    //Actualizar un usuario existente
-    app.put('/users/:id', (request, response) => {
-        const id = request.params.id;
-        pool.query('UPDATE users sET ? WHERE id = ?', [request.body, id], (error, result) => {
-            if (error) throw error;
-            response.send('User updated successfully.');
-        });
+  });
+
+  //mostrar un solo usuario por id
+
+  app.get('/users/:id', (request, response) =>{
+const id =request.params.id;
+pool.query('SELECT * FROM users WHERE id=?', id, (error, result)=>{
+if (error) throw error; 
+  
+response.send(result);
+
+});
+
+  });
+
+  //agregar un nvo usuario
+  app.post('/users/:id', (request, response) =>{
+  pool.query('INSERT INTO users SET ?',request.body, (error, result) =>{
+    if (error) throw error;
+    response.status(201).send(`User added with id:  ${result.insertId}`);
+
+    
+  });
+});
+
+//actualizar un usuario existente
+app.put('/users/:id', (request, response)=>{
+const id = request.params.id;
+
+pool.query('UPDATE users SET ? WHERE id=?',[request.body,id], (error, result) =>{
+  if (error) throw error;
+  response.send('user updated');
+    
+  });
+
+
+});
+
+//eliminar usuario
+app.delete('/users/:id', (request, response)=>{
+  const id = request.params.id;
+  
+  pool.query('DELETE FROM users WHERE id=?',id, (error, result) =>{
+    if (error) throw error;
+    response.send('user updated');
+      
     });
-    //mostrar todos los usuarios
-    app.delete('/users/:id', (request, response) => {
-        const id = request.params.id;
-        pool.query('DELETE FROM users WHERE id = ?', id,(error, result) => {
-            if (error) throw error;
-            response.send('User deleted.');
-        });
-    });
+  
+  
+  });
 }
 // exportar el router
 module.exports= router;
