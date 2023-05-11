9f67d8c5eb9c2a8dce55fd20f8dcbc55b6cf5923
Luis David Cazares Morales <luisdavidcazares@gmail.com>
2023-05-11 00:23:32 -0600

    creacion de rutas para la tabla productos

--- routes/routes.js
+++ routes/routes.js
@@ -2,10 +2,10 @@ const pool = require('../data/config');
 
 //ruta de la app
 const router = app => {
-    //mostrar mensaje de bienvenida de rrot
+    //mostrar mensaje de bienvenida de root
     app.get('/',(request, response) => {
         response.send({
-            message: 'Bienvenido a Node.js Express REST API!'
+            message: 'Bienvenido Luis David Cazares Morales!'
         });
     });
      //Mostrar todos los usuarios
@@ -59,7 +59,66 @@ app.delete('/users/:id', (request, response)=>{
   
   pool.query('DELETE FROM users WHERE id=?',id, (error, result) =>{
     if (error) throw error;
-    response.send('user updated');
+    response.send('user eliminated');
+      
+    });
+  
+  
+  });
+
+  //AQUI VA LO DE LA TABLA PRODUCTOS
+   //Mostrar todos los products
+   app.get("/products", (request, response) => {
+    pool.query("SELECT * FROM products", (error, result) => {
+      if (error) throw error;
+
+      response.send(result);
+    });
+  });
+
+  //mostrar un solo products por id
+
+  app.get('/products/:id', (request, response) =>{
+const id =request.params.id;
+pool.query('SELECT * FROM products WHERE id=?', id, (error, result)=>{
+if (error) throw error; 
+  
+response.send(result);
+
+});
+
+  });
+
+  //agregar un nvo products
+  app.post('/users/:id', (request, response) =>{
+  pool.query('INSERT INTO users SET ?',request.body, (error, result) =>{
+    if (error) throw error;
+    response.status(201).send(`products added with id:  ${result.insertId}`);
+
+    
+  });
+});
+
+//actualizar un products existente
+app.put('/users/:id', (request, response)=>{
+const id = request.params.id;
+
+pool.query('UPDATE products SET ? WHERE id=?',[request.body,id], (error, result) =>{
+  if (error) throw error;
+  response.send('products updated');
+    
+  });
+
+
+});
+
+//eliminar usuario
+app.delete('/products/:id', (request, response)=>{
+  const id = request.params.id;
+  
+  pool.query('DELETE FROM products WHERE id=?',id, (error, result) =>{
+    if (error) throw error;
+    response.send('product eliminated');
       
     });
   
